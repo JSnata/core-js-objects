accd0109220542b3d0cd086304e933a38cebd4aa
natagetup <n.kulikouskaya@gmail.com>
2024-02-27 15:16:00 +0100

    feat: add Rectangle task solution

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -178,8 +178,14 @@ function sellTickets(/* queue */) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  const rectangleObj = {};
+  rectangleObj.width = width;
+  rectangleObj.height = height;
+  rectangleObj.getArea = function () {
+    return this.width * this.height;
+  };
+  return rectangleObj;
 }
 
 /**
